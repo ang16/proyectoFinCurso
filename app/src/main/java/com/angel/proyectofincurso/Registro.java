fc7dac9dd1d2fb7b81bd025dd886e4e9d1c9c0fd
Ángel <angelgarcialopez@hotmail.com>
2019-05-30 10:24:23 +0200

    Prueba

--- app/src/main/java/com/angel/proyectofincurso/Registro.java
+++ app/src/main/java/com/angel/proyectofincurso/Registro.java
@@ -1,13 +1,28 @@
 package com.angel.proyectofincurso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Registro extends AppCompatActivity {
+
+    TextView registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        registro=findViewById(R.id.textregistroinicio);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(Registro.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
