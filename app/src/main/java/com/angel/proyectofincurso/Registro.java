e3c100faa39d70f3d4feaaba5b51b39236a7cf2c
Ángel <angelgarcialopez@hotmail.com>
2019-05-30 16:43:14 +0200

    Prueba

--- app/src/main/java/com/angel/proyectofincurso/Registro.java
+++ app/src/main/java/com/angel/proyectofincurso/Registro.java
@@ -1,6 +1,7 @@
 package com.angel.proyectofincurso;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,14 +16,42 @@ public class Registro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        registro=findViewById(R.id.textregistroinicio);
+        registro = findViewById(R.id.textregistroinicio);
 
         registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(Registro.this, LoginActivity.class);
+                Intent intent = new Intent(Registro.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
+
+
+    }
+
+    public void insertaUsuario() {
+        //Abrimos la base de datos 'DBUsuarios' en modo escritura
+        UsuarioDbHelper usdbh =
+                new UsuarioDbHelper(this, "ProyectoFinCurso", 1);
+
+        SQLiteDatabase db = usdbh.getWritableDatabase();
+
+        //Si hemos abierto correctamente la base de datos
+        if (db != null) {
+            //Insertamos 5 usuarios de ejemplo
+            for (int i = 1; i <= 5; i++) {
+                //Generamos los datos
+                int codigo = i;
+                String nombre = "Usuario" + i;
+
+                //Insertamos los datos en la tabla Usuarios
+                db.execSQL("INSERT INTO Usuarios (codigo, nombre) " +
+                        "VALUES (" + codigo + ", '" + nombre + "')");
+            }
+
+            //Cerramos la base de datos
+            db.close();
+
+        }
     }
 }
